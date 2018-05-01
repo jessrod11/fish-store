327312235f40829069471798fc42b9668cc452aa
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-04-30 21:12:11 -0500

    filtering through fish and toggling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,11 @@
 // Filter fish that are "on sale"
 
+const filterFish = () => { // no 'e' needs to be passed through because there won't be any dom traversal
+$('.fish').not('.on-sale').toggle(); // says to add or hide
+};
+
+$('#show-sale').click(filterFish);
+
 // Add fish to "Basket"
 const moveToCart = (e) => {
   let fishCard = $(e.target).closest('.fish');
